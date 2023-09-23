d3465a53b7ff0c009aee061eb833a17e1382e550
boomerangBS <106551310+boomerangBS@users.noreply.github.com>
2023-09-23 14:42:55 +0200

    Update and rename README.md to install.sh

--- README.md
+++
@@ -1,1 +0,0 @@
-# nodejs-installer2
---
+++ install.sh
@@ -0,0 +1,9 @@
+echo Installing NVM , please wait.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+echo Install Completed
+echo Configuring NVM
+source ~/.bashrc
+echo Installation successfull !
+echo use nvm install <version> to install a nodejs version and nvm use <version> to switch nodejs version
+echo THANK YOU FOR USING NVM INSTALLER !
+echo https://github.com/boomerangBS/nodejs-installer2/
